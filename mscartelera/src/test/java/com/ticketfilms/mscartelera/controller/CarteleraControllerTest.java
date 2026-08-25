16c9e7da092a1a966046926bc9a52decbc453e60
Vania <Van.carvajal@duocuc.cl>
2026-08-25 00:23:11 -0400

    Creación de pruebas unitarias

---
+++ mscartelera/src/test/java/com/ticketfilms/mscartelera/controller/CarteleraControllerTest.java
@@ -0,0 +1,5 @@
+package com.ticketfilms.mscartelera.controller;
+
+public class CarteleraControllerTest {
+    
+}
---
+++ mscartelera/src/test/java/com/ticketfilms/mscartelera/service/EventoServiceTest.java
@@ -0,0 +1,5 @@
+package com.ticketfilms.mscartelera.service;
+
+public class EventoServiceTest {
+    
+}
